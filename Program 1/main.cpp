f71bfb0b40872bde79dc85c6f19c32a8233acbbc
sarahsvitlik03 <sarahtheawesome03@gmail.com>
2025-02-03 09:50:43 -0500

    First commit

---
+++ Program 1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Program 1
+//
+//  Created by Sarah Svitlik on 2/3/25.
+//
+//
+
+#include <iostream>
+   
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
