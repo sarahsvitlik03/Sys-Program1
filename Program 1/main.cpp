9f0b1b700254f14c4f149ca836499fb0e6282ca8
sarahsvitlik03 <sarahtheawesome03@gmail.com>
2025-02-06 11:45:32 -0500

    began processing short switches

--- Program 1/main.cpp
+++ Program 1/main.cpp
@@ -10,19 +10,43 @@
 #include <getopt.h>
 using namespace std;
 
-int main(int argc, const char * argv[]) {
+int main(int argc, char * argv[]) {
     ofstream myOut ("P1_Svitlik_Larkin.txt", ios::out | ios::app);
     
     cout << "-----------------------------------------------" << endl;
     myOut << "-----------------------------------------------" << endl;
     
     int option;
-    struct option longOpts[] = {
+    struct option longOpts[] = { //Process long switches
         {"verbose", no_argument, NULL, 'b'},
         {"output", required_argument, NULL, 'o'},
+        {"recursive", no_argument, NULL, 'R'},
         {"debug", optional_argument, NULL, 0},
         {NULL, 0, NULL, 0}
     };
+    
+    cout << "Processing: " << argv[0] << endl;
+    int code, ch, optx;
+    
+    for (;;) { //Loop processes short switches
+        ch = getopt_long(argc, argv, "i:auiRlsb:o", longOpts, &code);
+        if (ch == -1) break;
+        switch (ch) {
+            case 'a': //these switches have no arguments.
+            case 'u':
+            case 'b':
+            case 'R': // Something
+            case 'o': // something
+            case 'i': //something
+                break;
+            case 0: {
+                //some code here
+                break;
+            }
+            case '?': //check  for invalid switches
+                break;
+        }
+    }
     
     return 0;
 }
